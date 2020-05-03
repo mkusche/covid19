bbe81148986f24733f3c8671a204dd5ae2accb24
michael.kusche <kusche@nupis.de>
2020-05-03 20:58:38 +0200

    compile

---
+++ build/precache-manifest.79d151578d939d19fbd28e1fbf72883e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5683777529732c24dc8521c659646db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d56d7fcb56694fbddb",
+    "url": "/static/css/2.269731ae.chunk.css"
+  },
+  {
+    "revision": "655b7ce07dcca819e673",
+    "url": "/static/css/main.be7c27d7.chunk.css"
+  },
+  {
+    "revision": "57d56d7fcb56694fbddb",
+    "url": "/static/js/2.05d9e2ce.chunk.js"
+  },
+  {
+    "revision": "0b878b27a8174ebc831edb82a56065a1",
+    "url": "/static/js/2.05d9e2ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655b7ce07dcca819e673",
+    "url": "/static/js/main.66bd0bb9.chunk.js"
+  },
+  {
+    "revision": "10543ac50263c3da14af",
+    "url": "/static/js/runtime-main.2e5ba990.js"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/static/media/dxicons.2b145cef.woff"
+  }
+]);
